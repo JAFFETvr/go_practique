e93f72ea17d0e677ec2e00375bbc055037cb6319
Mayito vicente <cv2143304@gmail.com>
2025-02-04 12:10:32 -0600

    primer intento de replica

--- main.go
+++ main.go
@@ -1,1 +1,101 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+	User string `json:"user"`
+}
+
+var users []User
+var mu sync.Mutex
+var nextID uint = 1
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	mu.Lock()
+	user.ID = nextID
+	nextID++
+	users = append(users, user)
+	mu.Unlock()
+
+	json.NewEncoder(w).Encode(user)
+	replicateData()
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var updatedUser User
+	json.NewDecoder(r.Body).Decode(&updatedUser)
+
+	mu.Lock()
+	defer mu.Unlock()
+	for i, u := range users {
+		if fmt.Sprintf("%d", u.ID) == params["id"] {
+			users[i] = updatedUser
+			users[i].ID = u.ID
+			json.NewEncoder(w).Encode(users[i])
+			replicateData()
+			return
+		}
+	}
+	http.Error(w, "User not found", http.StatusNotFound)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	mu.Lock()
+	defer mu.Unlock()
+	for i, u := range users {
+		if fmt.Sprintf("%d", u.ID) == params["id"] {
+			users = append(users[:i], users[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			replicateData()
+			return
+		}
+	}
+	http.Error(w, "User not found", http.StatusNotFound)
+}
+
+func replicateData() {
+	mu.Lock()
+	jsonData, _ := json.Marshal(users)
+	mu.Unlock()
+
+	resp, err := http.Post("http://localhost:5001/sync", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println("Error replicating data:", err)
+		return
+	}
+	defer resp.Body.Close()
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/users", createUser).Methods("POST")
+	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+
+	fmt.Println("Server running on port 5000")
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
--- src/replica/server_replica.go
+++ src/replica/server_replica.go
@@ -1,1 +1,47 @@
-package replica
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+	User string `json:"user"`
+}
+
+var users []User
+var mu sync.Mutex
+
+func syncUsers(w http.ResponseWriter, r *http.Request) {
+	var receivedUsers []User
+	json.NewDecoder(r.Body).Decode(&receivedUsers)
+
+	mu.Lock()
+	users = receivedUsers
+	mu.Unlock()
+
+	fmt.Println("Data replicated successfully")
+	w.WriteHeader(http.StatusOK)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	json.NewEncoder(w).Encode(users)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/sync", syncUsers).Methods("POST")
+	r.HandleFunc("/users", getUsers).Methods("GET")
+
+	fmt.Println("Replica server running on port 5001")
+	log.Fatal(http.ListenAndServe(":5001", r))
+}
